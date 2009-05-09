8951d4a8c3ec6d5234b754dc295712045b8980fa
Adam Kennedy <adamk@cpan.org>
2009-05-09 21:45:19 +0000

    Import of ADAMK/Perl-Metrics2-0.02 from CPAN.
    gitpan-cpan-distribution: Perl-Metrics2
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         ADAMK/Perl-Metrics2-0.02.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/Perl/Metrics2.pm
+++ lib/Perl/Metrics2.pm
@@ -68,13 +68,14 @@ use File::Spec             ();
 use File::HomeDir          ();
 use File::ShareDir         ();
 use File::Find::Rule       ();
+use File::Find::Rule::VCS  ();
 use File::Find::Rule::Perl ();
 use Params::Util           ();
 use PPI::Util              ();
 use PPI::Document          ();
 use Module::Pluggable;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use constant ORLITE_FILE => File::Spec->catfile(
 	File::HomeDir->my_data,
@@ -93,7 +94,7 @@ use ORLite::Migrate 0.03 {
 	file         => ORLITE_FILE,
 	create       => 1,
 	timeline     => ORLITE_TIMELINE,
-	user_version => 1,
+	user_version => 2,
 };
 
 
@@ -102,6 +103,30 @@ use ORLite::Migrate 0.03 {
 
 #####################################################################
 # Main Methods
+
+sub process_distribution {
+	my $class = shift;
+
+	# Get and check the directory name
+	my $path = File::Spec->canonpath(shift);
+	unless ( defined Params::Util::_STRING($path) ) {
+		Carp::croak("Did not pass a file name to index_file");
+	}
+	unless ( File::Spec->file_name_is_absolute($path) ) {
+		Carp::croak("Cannot index relative path '$path'. Must be absolute");
+	}
+	Carp::croak("Cannot index '$path'. File does not exist") unless -d $path;
+	Carp::croak("Cannot index '$path'. No read permissions") unless -r _;
+
+	# Find the documents
+	my @files = File::Find::Rule->ignore_svn->no_index->perl_file->in($path);
+	$class->trace("$path: Found " . scalar(@files) . " files");
+	foreach my $file ( @files ) {
+		$class->trace($file);
+		$class->process_file($file);
+	}
+	return 1;
+}
 
 sub process_file {
 	my $class = shift;
@@ -120,15 +145,55 @@ sub process_file {
 	# Load the document
 	my $document = PPI::Document->new( $path,
 		readonly => 1,
-	) or die("Failed to parse '$path'");
+	);
+	unless ( $document ) {
+		 warn("Failed to parse '$path'");
+		 next;
+	}
 
 	# Create the plugin objects
 	foreach my $plugin ( $class->plugins ) {
-		$class->trace("STARTING PLUGIN $plugin");
+		# $class->trace("STARTING PLUGIN $plugin");
 		eval "require $plugin";
 		die $@ if $@;
 		$plugin->new->process_document($document);
 	}
+
+	return 1;
+}
+
+sub index_distribution {
+	my $class = shift;
+	my $dist  = shift;
+	my $path  = shift;
+
+	# Find the documents
+	my @files = File::Find::Rule->ignore_svn
+		->no_index
+		->perl_file
+		->relative
+		->in($path);
+
+	# Generate the md5 checksums for the files
+	my %md5 = map {
+		$_ => PPI::Util::md5hex_file(
+			File::Spec->catfile($path, $_)
+		)
+	} @files;
+
+	# Flush and push the files into the database
+	Perl::Metrics2->begin;
+	Perl::Metrics2::CpanFile->delete(
+		'where dist = ?', $dist,
+	);
+	foreach my $file ( @files ) {
+		Perl::Metrics2::CpanFile->create(
+			dist => $dist,
+			file => $file,
+			md5  => $md5{$file},
+		);
+	}
+	Perl::Metrics2->commit;
 
 	return 1;
 }
